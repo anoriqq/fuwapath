df8c7c9ef7807ec4cf231646790abfe00a600336
anoriqq <shota.yoshikawa@anoriqq.com>
2019-08-08 00:00:11 +0900

    feat: エラーページにメッセージなどを表示するように

--- src/server/index.ts
+++ src/server/index.ts
@@ -54,7 +54,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
-  res.render('error', {title: 'Sorry'});
+  res.render('error', {title: 'Sorry', err});
 });
 
 app.listen(8000, (req: Request, res: Response, next: NextFunction) => {
