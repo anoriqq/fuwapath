ac358f7e4a918503ab3e32dee750aa961fb8be84
anoriqq <shota.yoshikawa@anoriqq.com>
2019-08-17 22:21:59 +0900

    chore: デフォルトの設定を仮追加

---
+++ src/config/default.js
@@ -0,0 +1,19 @@
+module.exports.CONFIG = {
+  /* 基本情報 */
+  NAME: 'fuwapath',
+  VERSION: '0.0.0',
+  MODE: '',
+
+  /* サーバー起動情報 */
+  SERVER: {
+    CONSOLE: {
+      PORT: 8000,
+    },
+    TRACK: {
+      PORT: 8010,
+    },
+  },
+
+  /* ロール */
+  ROLES: ['console', 'track'],
+};
