6236775f9a0d49d17d9e9167f336dd0ab25a5983
anoriqq <shota.yoshikawa@anoriqq.com>
2019-08-17 22:24:09 +0900

    chore: global設定の型定義を追加

--- src/typings/globals.d.ts
+++ src/typings/globals.d.ts
@@ -1,5 +1,21 @@
 type Config = {
-  name: string;
+  /* 基本情報 */
+  NAME: string;
+  VERSION: string;
+  MODE: string;
+
+  /* サーバー起動情報 */
+  SERVER: {
+    CONSOLE: {
+      PORT: number;
+    };
+    TRACK: {
+      PORT: number;
+    };
+  };
+
+  /* ロール */
+  ROLES: string[];
 };
 
 // eslint-disable-next-line no-unused-vars
