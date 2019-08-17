4f94ff67ad340f66106b32816f47e5df69f73565
anoriqq <shota.yoshikawa@anoriqq.com>
2019-08-17 22:40:12 +0900

    style: 変数宣言位置をブロック内に変更

--- src/server/runWorker.ts
+++ src/server/runWorker.ts
@@ -3,14 +3,17 @@ import {cpus} from 'os';
 import {debug} from 'debug';
 import {run} from './console';
 
-const numCPUs = cpus().length;
-/* logger */
-const workerLog = debug('worker');
-
 if (cluster.isMaster) {
   /* マスタープロセス */
+
+  /* logger */
+  const workerLog = debug('worker');
+
   workerLog(`Master ${process.pid} is running`);
+
+  const numCPUs = cpus().length;
   for (let i = 0; i < numCPUs; i++) cluster.fork();
+
   cluster.on('exit', (worker, code, signal) => {
     workerLog(`Worker ${worker.process.pid} died`);
     cluster.fork();
